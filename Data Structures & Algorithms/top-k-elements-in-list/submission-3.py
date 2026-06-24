62334d9a736852657516b4a4d0135dd59b81f554
MaruthiEranki <60032318+MaruthiEranki@users.noreply.github.com>
2026-06-24 08:06:59 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        nums_set = set(nums)
+        indices = []
+        for each_num in nums_set:
+            indices.append([index for index, value in enumerate(nums) if value == each_num])
+
+        indices.sort(key=len, reverse=True)
+        
+        indices = indices[:k]
+        print(indices)
+        for indx, each_indx in enumerate(indices):
+            indices[indx] = nums[indices[indx][0]]
+        print(indices)
+        return indices
